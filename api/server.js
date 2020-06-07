43c5df82fbc623190b07f6e905b3386a200bbca8
Gabriel Romero <gabester78@gmail.com>
2020-06-07 15:00:24 -0500

    CRUD working.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const db = require("../data/dbConfig.js");
-
 const server = express();
+const accountsRouter = require("../data/accountsRouter.js");
 
 server.use(express.json());
+server.use("/accounts", accountsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>It's alive!!!</h2>`);
+});
 
 module.exports = server;
